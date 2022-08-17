4d7ce21e6e80eb279ad68b1b13e2b7a0d36de06e
João Volpato <volpatocode@gmail.com>
2022-08-17 19:19:27 -0300

    fix: bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import SingleProduct from "../components/SingleProduct";
 
-
 export default function index() {
-
-
   return (
     <>
-<SingleProduct img="./product.png" name="Nome aleatório" brand="Abibas" price="999,99"/>
+      <SingleProduct
+        img="./product.png"
+        name="Nome aleatório"
+        brand="Abibas"
+        price="999,99"
+      />
     </>
   );
 }
