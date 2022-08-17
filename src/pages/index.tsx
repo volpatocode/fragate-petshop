fb441ebc5799dd6dd3c6a8ef9055298422610ee1
João Volpato <volpatocode@gmail.com>
2022-08-17 20:26:15 -0300

    fix: icon conflict

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import Icons from "../components/Icons";
-import SingleProduct from "../components/SingleProduct";
 
 export default function index() {
   return (
     <>
-      <SingleProduct img="product.png" brand="abibas" name="Nome aleatório" price="999"/>
     </>
   );
 }
