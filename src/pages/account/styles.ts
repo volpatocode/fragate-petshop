47b97f96e717db8fa462d7c2842938f1abf5bddc
João Volpato <volpatocode@gmail.com>
2022-08-22 18:53:02 -0300

    changes: improved account structure

--- src/pages/account/styles.ts
+++ src/pages/account/styles.ts
@@ -6,7 +6,6 @@ export const Account = styled(Box)`
   flex-direction: row;
   width: 100vw;
   height: 100vh;
-  background-color: #007FE8;
   align-items: center;
   justify-content: center;
   column-gap: 10rem;
@@ -15,6 +14,9 @@ export const Account = styled(Box)`
 export const AccountFormWrapper = styled(Box)`
   width: 50vw;
   height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const AccountImageWrapper = styled(Box)`
@@ -23,14 +25,66 @@ export const AccountImageWrapper = styled(Box)`
   display: flex;
 `;
 
-export const AccountForm = styled.form``;
+export const AccountImage = styled.img`
+  width: 100%;
+`;
 
-export const InputBox = styled(Box)``;
+export const AccountForm = styled.form`
+  background-color: #fff;
+  border-radius: 15px;
+  width: 75%;
+  padding: 5rem 3rem;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.3rem;
+`;
 
-export const Input = styled.input``;
+export const Logo = styled.h2`
+  text-align: center;
+  font-size: 2rem;
+  letter-spacing: 0.3px;
+  color: #ff9a57;
+  line-height: 1.65;
+`;
 
-export const SubmitButton = styled(Button)``;
+export const InputBox = styled(Box)`
+  width: 100%;
+`;
 
-export const SignWithMethodBox = styled(Box)``;
+export const Input = styled.input`
+  background-color: rgba(255, 255, 255, 1);
+  color: rgba(0, 0, 0, 0.8);
+  padding: 12px 10px;
+  border-radius: 8px;
+  border: 1px solid #707070;
+  outline: none;
+  width: 100%;
+`;
+
+export const AnchorInfo = styled.a`
+  color: #ff9a57;
+  font-size: 0.8rem;
+`;
+
+export const SubmitButton = styled(Button)`
+  background-color:  #ff9a57;
+  color: rgba(255, 255, 255, 1);
+  width: 100%;
+  :hover {
+    background-color:  #ff9a57;
+  }
+  text-transform: none;
+`;
+
+export const SignWithMethodBox = styled(Box)`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  
+`;
 
 export const SignWithButton = styled(Button)``;
