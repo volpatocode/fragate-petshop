7a97074cf30a615bbc31b113f438a588280e2935
PedroCaprioli <pedrocaprioli02@gmail.com>
2022-08-25 05:30:33 -0300

    changes: dots styles

--- src/components/ShowcaseCarousel/index.tsx
+++ src/components/ShowcaseCarousel/index.tsx
@@ -5,42 +5,33 @@ import { Img, ShowcaseCarousel } from "./styles";
 
 export default function index() {
   const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 10000, min: 3000 },
-      items: 1,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
+    singleItem: {
+      breakpoint: { max: 10000, min: 1 },
       items: 1,
     },
   };
 
   return (
-    <ShowcaseCarousel>
-      <Carousel
-        responsive={responsive}
-        showDots={true}
-        renderDotsOutside={false}
-        dotListClass="dots"
-        autoPlay={true}
-        arrows={false}
-        infinite={true}
-        autoPlaySpeed={5000}
-      >
-        <Img src="./Showcase1.svg" />
-        <Img src="./Showcase2.svg" />
-        <Img src="./Showcase3.svg" />
-        <Img src="./Showcase4.svg" />
-        <Img src="./Showcase5.svg" />
-      </Carousel>
-    </ShowcaseCarousel>
+    <>
+      <ShowcaseCarousel>
+        <Carousel
+          responsive={responsive}
+          showDots={true}
+          renderDotsOutside={false}
+          dotListClass="dots"
+          autoPlay={true}
+          arrows={false}
+          infinite={true}
+          autoPlaySpeed={5000}
+        >
+          <Img src="./Showcase1.svg" />
+          <Img src="./Showcase2.svg" />
+          <Img src="./Showcase3.svg" />
+          <Img src="./Showcase4.svg" />
+          <Img src="./Showcase5.svg" />
+        </Carousel>
+      </ShowcaseCarousel>
+      
+    </>
   );
 }
--- src/components/ShowcaseCarousel/styles.ts
+++ src/components/ShowcaseCarousel/styles.ts
@@ -1,19 +1,23 @@
-
-import styled from "styled-components"
+import styled from "styled-components";
 import { Box } from "@mui/material";
 
-
 export const Img = styled.img`
-    width: 100%;
-`
+  width: 100%;
+`;
 
 export const ShowcaseCarousel = styled(Box)`
-    .dots{
-        padding-bottom:1rem;
-        button{
-            border: none; 
-            
-        }
-        
-     }
-`
+  .dots {
+    padding-bottom: 1rem;
+    button {
+      border: none;
+    }
+  }
+
+  .react-multi-carousel-dot button {
+    background-color: transparent;
+    border: 1px solid rgba(0, 0, 0, 0.4);
+  }
+  .react-multi-carousel-dot--active button {
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+`;
